e547a42ba5966d6757c1b25f6194dd3cce175d9d
onkar vasav <onkarvasav@onkars-MacBook-Air.local>
2025-01-20 18:11:22 +0530

    add single user view modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ setModal, id }) => {
+  const [singleUserData, setSingleUserData] = useState();
+  const { users } = useSelector((state) => state.app);
+  console.log("idd", id);
+  console.log(users);
+  const singleUser = users.find((user) => user.id === id);
+
+  console.log("single ", singleUser);
+
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
+          {/* Modal Header */}
+          <div className="flex justify-between items-center border-b pb-3">
+            <h3 className="text-lg font-semibold">User Details</h3>
+            <button
+              onClick={() => setModal(false)}
+              className="text-gray-500 hover:text-gray-700"
+            >
+              ✕
+            </button>
+          </div>
+
+          {/* Modal Body */}
+          <div className="mt-4">
+            <p className="text-gray-600">Name : {singleUser.name}</p>
+          </div>
+          <div className="mt-4">
+            <p className="text-gray-600">Age : {singleUser.age}</p>
+          </div>
+          <div className="mt-4">
+            <p className="text-gray-600">City : {singleUser.city}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { deleteData } from "../redux/slices/userSlice";
 import { useNavigate } from "react-router";
+import Modal from "./Modal";
 
 const User = ({ user }) => {
+  const [modal, setModal] = useState(false);
+  const [id, setId] = useState();
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleUserDelete = (id) => {
@@ -14,38 +17,48 @@ const User = ({ user }) => {
   const handleEditRedirect = (id) => {
     navigate(`/edit/${id}`);
   };
+
+  const handleViewUser = (id) => {
+    setModal(true);
+    setId(id);
+  };
+
   return (
-    <tr className="border-b hover:bg-orange-100 bg-gray-100">
-      <td className="p-3 px-5">{user.name}</td>
-      <td className="p-3 px-5">{user.age}</td>
-      <td className="p-3 px-5">{user.city}</td>
-      <td className="p-3 px-5 flex justify-end">
-        <button
-          type="button"
-          className="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-        >
-          View
-        </button>
-        <button
-          type="button"
-          className="mr-3 text-sm bg-orange-500 hover:bg-orange-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-          onClick={() => {
-            handleEditRedirect(user.id);
-          }}
-        >
-          Edit
-        </button>
-        <button
-          onClick={() => {
-            handleUserDelete(user.id);
-          }}
-          type="button"
-          className="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-        >
-          Delete
-        </button>
-      </td>
-    </tr>
+    <>
+      <tr className="border-b hover:bg-orange-100 bg-gray-100">
+        <td className="p-3 px-5">{user.name}</td>
+        <td className="p-3 px-5">{user.age}</td>
+        <td className="p-3 px-5">{user.city}</td>
+        <td className="p-3 px-5 flex justify-end">
+          <button
+            type="button"
+            className="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => handleViewUser(user.id)}
+          >
+            View
+          </button>
+          <button
+            type="button"
+            className="mr-3 text-sm bg-orange-500 hover:bg-orange-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => {
+              handleEditRedirect(user.id);
+            }}
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => {
+              handleUserDelete(user.id);
+            }}
+            type="button"
+            className="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+      {modal && <Modal setModal={setModal} id={id} />}
+    </>
   );
 };
 
